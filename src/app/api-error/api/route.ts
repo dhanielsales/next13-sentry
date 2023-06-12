f36549c03884dea4de3524508cee9b8d28573bc9
Dhaniel Sales <dhanielr94@gmail.com>
2023-06-12 14:50:31 -0300

    initial commit

--- src/app/api-error/api/route.ts
+++ src/app/api-error/api/route.ts
@@ -2,5 +2,5 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_resquest: NextRequest) {
   // throw new Error("Sentry Example API Route Error");
-  NextResponse.json({ name: "John Doe" });
+  return NextResponse.json({ name: "John Doe" });
 }
